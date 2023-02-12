986259540fddbae25b114a9b5eb2577d1205b067
Artem G <itemun@gmail.com>
2023-02-12 19:58:26 +0300

    refa: add ProductStorage int

--- internal/core/models.go
+++ internal/core/models.go
@@ -1,5 +1,15 @@
 package core
 
+import (
+	"context"
+	"fmt"
+)
+
+type ProductStorage interface {
+	IncreaseProductCount(ctx context.Context, productID string, delta int) error
+	DecreaseProductCount(ctx context.Context, productID string, delta int) error
+}
+
 type Product struct {
 	Name     string
 	Price    int
@@ -7,44 +17,29 @@ type Product struct {
 }
 
 type Shop struct {
-	Products map[string]*Product
+	productStorage ProductStorage
 }
 
-func NewShop() *Shop {
+func NewShop(storage ProductStorage) *Shop {
 	return &Shop{
-		Products: map[string]*Product{
-			"1": {
-				Name:     "Phone",
-				Price:    50000,
-				Quantity: 100,
-			},
-			"2": {
-				Name:     "TV",
-				Price:    100000,
-				Quantity: 50,
-			},
-			"3": {
-				Name:     "Notebook",
-				Price:    150000,
-				Quantity: 30,
-			},
-		}}
+		productStorage: storage,
+	}
 }
 
-func (s *Shop) Sell(id string) error {
-	product, ok := s.Products[id]
-	if !ok || product.Quantity == 0 {
-		return ErrProductNotFound
+func (s *Shop) Sell(ctx context.Context, id string) error {
+	err := s.productStorage.DecreaseProductCount(ctx, id, 1)
+	if err != nil {
+		return fmt.Errorf("can't decrease product count: %w", err)
 	}
-	product.Quantity--
+
 	return nil
 }
 
-func (s *Shop) Buy(id string) error {
-	product, ok := s.Products[id]
-	if !ok || product.Quantity == 0 {
-		return ErrProductNotFound
+func (s *Shop) Buy(ctx context.Context, id string) error {
+	err := s.productStorage.IncreaseProductCount(ctx, id, 1)
+	if err != nil {
+		return fmt.Errorf("can't increase product count: %w", err)
 	}
-	product.Quantity++
+
 	return nil
 }
